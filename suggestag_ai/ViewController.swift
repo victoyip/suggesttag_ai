72478c07ea52a8937e627bee247f65f41adcbf7e
Jessica Joseph <Jessicajoseph@nyu.edu>
2016-01-30 11:28:20 -0500

    The Creation of Suggestag.ai

---
+++ suggestag_ai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  suggestag_ai
+//
+//  Created by Jessica Joseph on 1/30/16.
+//  Copyright © 2016 HackCamTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
